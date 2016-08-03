87e68195b22ce6c11f822bc0b6899667ee4ee4b3
马文帅 <mawenshuai100@163.com>
2016-08-03 13:12:49 +0800

    实现了iOS中钟表的效果

---
+++ ClockExample/ClockExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClockExample
+//
+//  Created by 马文帅 on 16/8/3.
+//  Copyright © 2016年 mawenshuai. All rights reserved.
+//  代码详解:http://www.jianshu.com/p/504f37e93533
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
